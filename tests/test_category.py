fa331fd99bab1b9352e2e4fa6216cf171b3da3af
Sergey-Molchan <Molchan2025@gmail.com>
2025-06-27 10:22:03 -0700

    добавил тесты к основной структуре

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,2 +1,8 @@
-import pytest
 from marketplace.category import Category
+from marketplace.product import Product
+
+def test_category_init():
+    product = Product("Test", "Desc", 100.0, 5)
+    category = Category("Test Category", "Desc", [product])
+    assert category.name == "Test Category"
+    assert len(category.products) == 1
