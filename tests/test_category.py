8d1bed3f0a1bc5738379d073fdbe8397e8bb68ee
Sergey-Molchan <Molchan2025@gmail.com>
2025-06-27 10:32:56 -0700

    Доработаны и исправлены тесты,теперь завершаются прпвильно

--- tests/test_category.py
+++ tests/test_category.py
@@ -5,4 +5,6 @@ def test_category_init():
     product = Product("Test", "Desc", 100.0, 5)
     category = Category("Test Category", "Desc", [product])
     assert category.name == "Test Category"
+    assert category.description == "Desc"
     assert len(category.products) == 1
+    assert isinstance(category.products[0], Product)
